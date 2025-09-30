3583693d1f91d30972bd875f2103ec10e0490045
Seamus Johnson <SMJohnson6@student.fullsail.edu>
2025-09-30 13:56:55 -0400

    Started PB collections setup

--- src/pocketbase_access.py
+++ src/pocketbase_access.py
@@ -1,18 +1,30 @@
 from pocketbase import PocketBase
 
-import os, subprocess
+import os, subprocess, atexit
 from dotenv import load_dotenv
 
 PB_FOLDER = "pocketbase"
 
 # Load environment variables from .env
 load_dotenv()
-POCKETBASE_URL = os.getenv("POCKETBASE_URL", "http://localhost/8090")
+POCKETBASE_URL = os.getenv("POCKETBASE_URL", "http://localhost:8090")
 PB_ADMIN_EMAIL = os.getenv("PB_ADMIN_EMAIL", "")
 PB_ADMIN_PASSWORD = os.getenv("PB_ADMIN_PASSWORD", "")
 
 # Start and connect
-subprocess.run([f"{PB_FOLDER}/pocketbase.exe"])
+pb = subprocess.Popen([f"{PB_FOLDER}/pocketbase.exe", "serve"])
 
-client = PocketBase(f"POCKETBASE_URL")
-client.admins.auth_with_password("PB_ADMIN_EMAIL", "PB_ADMIN_PASSWORD")
+def pb_close():
+    print("Stopping PB...")
+    pb.terminate()
+    pb.wait()
+    print("PB Stopped!")
+
+atexit.register(pb_close)
+
+client = PocketBase(POCKETBASE_URL)
+client.admins.auth_with_password(PB_ADMIN_EMAIL, PB_ADMIN_PASSWORD)
+
+if __name__ == "__main__":
+    while True:
+        pass
